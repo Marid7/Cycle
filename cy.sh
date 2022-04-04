2e32d9766033bba8695aeae9376a02a9414b4b9d
Marid7 <100313410+Marid7@users.noreply.github.com>
2022-04-05 01:20:34 +0600

    Create cy.sh

---
+++ cy.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+#color
+orng="\033[1;33m"
+cyan="\033[1;36m"
+red="\033[1;31m"
+grn="\033[1;92;44m"
+blk="\033[1;30m"
+wht="\033[1;37m"
+nc="\033[0m"
+rd="\033[1;93;44m"
+#bg
+
+obg="\033[1;93;46m"
+bbg="\033[1;93;46m"
+wbg="\033[1;97;46m"
+gbg="\033[1;92;46m"
+
+
+while true;do
+ echo -e "___________________________________"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${obg}                         (@)     ${nc}|"
+ echo -e "|${wbg}                                (${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (-)-----(-)             ${nc}|"
+ echo -e "|${rd}oooooOoooooooooooOoooooooooooOooo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${bbg}                                ${wbg}_${nc}|"
+ echo -e "|${obg}                        ( @  ${wbg} _(&${nc}|"
+ echo -e "|${wbg}                             (&&&${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (\)-----(\)             ${nc}|"
+ echo -e "|${rd}ooooOoooooooooooOoooooooooooOoooo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}                            __   ${nc}|"
+ echo -e "|${obg}                           ${wbg}(&&)__${nc}|"
+ echo -e "|${wbg}                         (&&&&&&&${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (|)-----(|)             ${nc}|"
+ echo -e "|${rd}oooOoooooooooooOoooooooooooOooooo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}                      __         ${nc}|"
+ echo -e "|${wbg}                    _(&&)_${obg})      ${nc}|"
+ echo -e "|${wbg}                   (&&&&&&&)     ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (/)-----(/)             ${nc}|"
+ echo -e "|${rd}ooOoooooooooooOoooooooooooOoooooo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}              __                 ${nc}|"
+ echo -e "|${wbg}            _(&&)__      ${obg}(@)     ${nc}|"
+ echo -e "|${wbg}           (&&&&&&&)             ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (-)-----(-)             ${nc}|"
+ echo -e "|${rd}oOoooooooooooOoooooooooooOooooooo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}          __                     ${nc}|"
+ echo -e "|${wbg}        _(&&)__         ${obg}( @ )    ${nc}|"
+ echo -e "|${wbg}       (&&&&&&&)                 ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (\)-----(\)             ${nc}|"
+ echo -e "|${rd}OoooooooooooOoooooooooooOoooooooo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}      __                         ${nc}|"
+ echo -e "|${wbg}    _(&&)__              ${obg}(@)     ${nc}|"
+ echo -e "|${wbg}   (&&&&&&&)                     ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (|)-----(|)             ${nc}|"
+ echo -e "|${rd}oooooooooooOoooooooooooOooooooooo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}  __                             ${nc}|"
+ echo -e "|${wbg}_(&&)__                 ${obg}( @ )    ${nc}|"
+ echo -e "|${wbg}&&&&&&&)                         ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (/)-----(/)             ${nc}|"
+ echo -e "|${rd}ooooooooooOoooooooooooOoooooooooO${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}_                                ${nc}|"
+ echo -e "|${wbg}&)__                     ${obg}(@)     ${nc}|"
+ echo -e "|${wbg}&&&&)                            ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (-)-----(-)             ${nc}|"
+ echo -e "|${rd}oooooooooOoooooooooooOoooooooooOo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}                                 ${nc}|"
+ echo -e "|${wbg}__                      ${obg}( @ )    ${nc}|"
+ echo -e "|${wbg}&&)                              ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (\)-----(\)             ${nc}|"
+ echo -e "|${rd}ooooooooOoooooooooooOoooooooooOoo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}                                 ${nc}|"
+ echo -e "|${obg}                         ${obg}(@)     ${nc}|"
+ echo -e "|${wbg}                                 ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (|)-----(|)             ${nc}|"
+ echo -e "|${rd}oooooooOoooooooooooOoooooooooOooo${nc}|"
+sleep 0.1
+clear
+ echo -e "___________________________________"
+ echo -e "|${wbg}                                 ${nc}|"
+ echo -e "|${obg}                        ${obg}( @ )    ${nc}|"
+ echo -e "|${wbg}                                 ${nc}|"
+ echo -e "|${bbg}                                 ${nc}|"
+ echo -e "|${grn}                _                ${nc}|"
+ echo -e "|${grn}          /-\   -\<              ${nc}|"
+ echo -e "|${grn}         (/)-----(/)             ${nc}|"
+ echo -e "|${rd}ooooooOoooooooooooOoooooooooOoooo${nc}|"
+sleep 0.1
+clear
+done
